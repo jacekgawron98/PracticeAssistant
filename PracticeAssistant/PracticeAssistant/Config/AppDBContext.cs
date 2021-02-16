00cf5a894cdd8a0f62d0b337a954fbba73e53c92
Jacek Gawron <gawron.jacek98@gmail.com>
2021-02-16 19:09:36 +0100

    Added users database tables

---
+++ PracticeAssistant/PracticeAssistant/Config/AppDBContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using PracticeAssistant.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PracticeAssistant.Config
+{
+    public class AppDBContext : IdentityDbContext<PAUser>
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
